bcafb5447baaebdffe187ac47af301919fe868e5
Chintan <chintanpathak85@yahoo.com>
2009-12-06 23:40:04 -0800

    querying

---
+++ doc/rest/tutorials/tutorials-pygr recipes/alignment/Using the �translated BLASTs� (blastx and tblastx)/transln3.py
@@ -0,0 +1,47 @@
+
+from pygr import seqdb
+
+from pygr import blast
+
+from pygr import cnestedlist
+
+from pygr import translationDB
+
+def main():
+    """This function returns argument
+    Example:
+    >>> main()
+    MVHLTDAEKA
+    WCT*LMLRRL
+    LVVLVSQGSG
+    """
+
+    dna_db = seqdb.SequenceFileDB('data/hbb1_mouse.fa')
+
+    translation_db = translationDB.get_translation_db(dna_db)
+
+    frame0 = translation_db[dna_seq.id][0:]
+
+    print str(frame0[:10])
+
+    frame1 = translation_db[dna_seq.id][1:]
+
+    print str(frame1[:10])
+
+    negframe0 = (-translation_db[dna_seq.id])[:0]
+
+    print str(negframe0[:10])
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
+
+
+
+
+
+
+
+
+
+
