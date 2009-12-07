82c8695593b4bf1fcc920d2868402453dc85c769
Chintan <chintanpathak85@yahoo.com>
2009-12-07 00:53:06 -0800

    1

---
+++ doc/rest/tutorials/tutorials-pygr recipes/Annotation2/slice_pickle_obj1.py
@@ -0,0 +1,59 @@
+class MySliceInfo(object):
+   def __init__(self, seq_id, start, stop, orientation):
+      (self.id, self.start, self.stop, self.orientation) = (seq_id, start, stop, orientation)
+
+class MyFunkySliceInfo(object):
+   def __init__(self, seq_id, begin, end, strand):
+       (self.seq_id, self.begin, self.end, self.strand) = (seq_id, begin, end, strand)
+
+from pygr import seqdb, annotation, worldbase, mapping, cnestedlist
+
+from slice_pickle_obj import MySliceInfo, MyFunkySliceInfo
+
+dna_db = seqdb.SequenceFileDB('data\hbb1_mouse.fa')
+
+seq_id = 'gi|171854975|dbj|AB364477.1|'
+
+def main():
+    """This function returns argument
+    Example:
+    >>> main()
+    'A'
+    True
+    -annotA[0:50]
+    annotA[0:50]
+    """
+    annodb = annotation.AnnotationDB({}, dna_db)
+
+    slice1 = MySliceInfo(seq_id, 0, 50, +1)
+
+    slice2 = MySliceInfo(seq_id, 300, 400, -1)
+
+    annot1 = annodb.new_annotation('A', slice1)
+
+    annot2 = annodb.new_annotation('B', slice2)
+
+    for k in annodb:
+        print repr(annodb[k]), repr(annodb[k].sequence)
+
+    a = annodb['A']
+
+    print a.id
+
+    print a.db is annodb
+
+    print str(a)
+
+    print -a
+
+    print (-a[10:20]).pathForward
+
+if __name__ == '__main__':
+   import doctest
+   doctest.testmod()
+
+
+
+
+
+
