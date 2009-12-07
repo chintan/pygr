bff653039cf76204f9260d0bdd3b68bb6578fab2
Chintan <chintanpathak85@yahoo.com>
2009-12-06 23:19:33 -0800

    aligned

---
+++ doc/rest/tutorials/tutorials-pygr recipes/alignment/Alignment Basics/wa1.py
@@ -0,0 +1,44 @@
+
+
+from pygr import seqdb, annotation
+
+from pygr import cnestedlist
+
+def main():
+    """This function returns argument.
+    Example:
+    >>> main()
+    [HBB1_RAT[42:62], HBB1_XENLA[38:58]]
+    """
+    simple_al = cnestedlist.NLMSA('hbb', mode='memory', pairwiseMode=True)
+
+    db = seqdb.SequenceFileDB('data/sp_all_hbb')
+
+    mouse = db['HBB1_MOUSE']
+
+    rat = db['HBB1_RAT']
+
+    frog = db['HBB1_XENLA']
+
+    simple_al += mouse
+
+    ival = mouse[40:60]
+
+    simple_al[ival] += rat[42:62]
+
+    simple_al[ival] += frog[38:58]
+
+    simple_al.build()
+
+    del simple_al
+
+    seqDict = seqdb.PrefixUnionDict({ 'sp_all_hbb': db })
+
+    loaded_al = cnestedlist.NLMSA('tempdir/hbb', seqDict=seqDict)
+
+    print loaded_al[ival].keys()
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
